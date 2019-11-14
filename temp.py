f40c651dba6343539152ec8692953bfccff0253f
TaeGyun Kim <tagun1202@gmail.com>
2019-11-14 18:42:15 +0900

    test case is solved. Need to improve accuracy

--- temp.py
+++ temp.py
@@ -1,6 +1,5 @@
 import numpy as np
 from math import inf
-import argparse
 from pathlib import Path
 import imageio
 from colormath.color_objects import LabColor, sRGBColor
@@ -8,6 +7,18 @@ from colormath.color_conversions import convert_color
 from colormath.color_diff import delta_e_cie1976
 import heapq
 
+ground_truth = {
+    "1": "SPRING",
+    "2": "AUTUMN",
+    "3": "Winter",
+    "4": "AUTUMN",
+    "5": "SUMMER",
+    "6": "SPRING",
+    "7": "WINTER",
+    "8": "WINTER",
+    "9": "WINTER",
+    "10": "WINTER"
+}
 seasons= ["spring","summer","autumn","winter"]
 types=["hair","eye"]
 
@@ -16,40 +27,17 @@ results={
     ("","brunette")
 }
 
-eyes={
-    "brown":(210,105,30),
-    "black":(0,0,0),
-    "blue": (57,115,220)
-}
+# ##http://www.inven.co.kr/board/heroes/2028/28035
 
-hairs={
-    "blonde":(255,255,0),
-    "black":(0,0,0),
-    "brunette":(116,42,15)
-}
-
-##http://www.inven.co.kr/board/heroes/2028/28035
-skins={
-    "apricot":(251,206,177),
-    "black":(120,92,69),
-    "white":(255,255,255)
-}
+eye_labels = dict()
+hair_labels = dict()
 
 eye_dic = dict()
 hair_dic = dict()
 skin_dic = dict()
 
-def find_smallest(rgb,R_avg,G_avg,B_avg):
-    tmp_red,tmp_green,tmp_blue = rgb 
-    return abs(tmp_red-R_avg) + abs(tmp_green - G_avg) + abs(tmp_blue - B_avg)
-
-def make_label_dict():
-    for season in seasons:
-        for ttype in types:
-            vars()[season+"_"+ttype] = dict()
-
-def output(color_type,dir):
-    rgb_img = imageio.imread(dir)
+def calc_lab(img_dir):
+    rgb_img = imageio.imread(img_dir)
 
     Red = []
     Green = []
@@ -70,87 +58,66 @@ def output(color_type,dir):
     
     rgb = sRGBColor(R_avg, G_avg, B_avg, is_upscaled=True)
     lab = convert_color(rgb, LabColor, through_rgb_type=sRGBColor)
+    return lab
 
-    #minimum = inf
-    ret =""
-    
+def make_label_dict():
+    directory = Path("/Users/kimtaegyun/colorization/detect/data/label")
+
+    for season in seasons:
+        for ttype in types:
+            label = season+"_"+ttype+".png"
+            img_dir = directory/label
+            if ttype == "hair":
+                hair_labels[season] = calc_lab(img_dir)
+            else:
+                eye_labels[season] = calc_lab(img_dir)    
+
+def output(color_type,img_dir):
+    lab = calc_lab(img_dir)
+
     if color_type=="eye":
-        for color, rgb in eyes.items():
-#            flag = minimum
-            tmp_r, tmp_g, tmp_b = rgb
-            tmp_rgb = sRGBColor(tmp_r, tmp_g, tmp_b, is_upscaled=True)
-            lab2 = convert_color(tmp_rgb, LabColor, through_rgb_type=sRGBColor)
-            eye_dic[color] = delta_e_cie1976(lab, lab2)
-#            minimum = min(minimum,delta_e_cie1976(lab, lab2))
-#            if minimum != flag:
-#                ret = color
-
+        for season, lab2 in eye_labels.items():
+            eye_dic[season] = delta_e_cie1976(lab, lab2)
     elif color_type=="hair":
-        for color, rgb in hairs.items():
-            flag = minimum
-            tmp_r, tmp_g, tmp_b = rgb
-            tmp_rgb = sRGBColor(tmp_r, tmp_g, tmp_b, is_upscaled=True)
-            lab2 = convert_color(tmp_rgb, LabColor, through_rgb_type=sRGBColor)
-            hair_dic[color]=delta_e_cie1976(lab, lab2)
-#            minimum = min(minimum,delta_e_cie1976(lab, lab2))
-            # if minimum != flag:
-            #     ret = color
-
-    elif color_type=="skin":
-        for color, rgb in skins.items():
-#            flag = minimum
-            tmp_r, tmp_g, tmp_b = rgb
-            tmp_rgb = sRGBColor(tmp_r, tmp_g, tmp_b, is_upscaled=True)
-            lab2 = convert_color(tmp_rgb, LabColor, through_rgb_type=sRGBColor)
-            skin_dic[color]=delta_e_cie1976(lab, lab2)
-            # minimum = min(minimum,delta_e_cie1976(lab, lab2))
-            # if minimum != flag:
-            #     ret = color
+        for season, lab2 in hair_labels.items():
+            hair_dic[season] = delta_e_cie1976(lab, lab2)
     else:
         print("Wrong color type")
         exit(0)
 
-#    return ret
-
 if __name__=='__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("eye")
-    parser.add_argument("skin")
-    parser.add_argument("hair")
-    args = parser.parse_args()
+    print("===================RESULT===================")
+    correct = 0 
+    wrong = 0
+    DATA_DIR =Path("/Users/kimtaegyun/colorization/detect/data/test")
+    for T in range(1,11):
+        eye = "eye/"+str(T)+".jpg"
+        hair = "hair/"+str(T)+".jpg"
+        EYE_DIR = DATA_DIR/eye
+        HAIR_DIR = DATA_DIR/hair
 
-    eye = args.eye+".png"
-    skin = args.skin+".png"
-    hair = args.hair+".png"
+        make_label_dict()
 
-    DATA_DIR =Path("/Users/kimtaegyun/colorization/detect/data/")
-    EYE_DIR = DATA_DIR/"eyes"/eye
-    SKIN_DIR = DATA_DIR/"skins"/skin
-    HAIR_DIR = DATA_DIR/"hairs"/hair
-#    eye = output("eye",EYE_DIR)
-#    skin = output("skin",SKIN_DIR)
-#    hair = output("hair",HAIR_DIR)
+        output("eye",EYE_DIR)
+        output("hair",HAIR_DIR)
 
-    output("eye",EYE_DIR)
-    output("skin",SKIN_DIR)
-    output("hair",HAIR_DIR)
+        priority_queue = []
+        for eye_clr, eye_v in eye_dic.items():
+            for hair_clr, hair_v in hair_dic.items():
+                heapq.heappush(priority_queue,(eye_v+hair_v,(eye_clr,hair_clr)))
 
-    print("Real Eye: ",args.eye)
-    print("Eye Prediction: ",eye)
-    print("Real Hair: ",args.skin)
-    print("Skin Prediction: ",skin)
-    print("Real Hair: ", args.hair)
-    print("Hair Prediction: ",hair)
-
-    priority_queue = []
-    for eye_clr, eye_v in eye_dic.items():
-        for hair_clr, hair_v in hair_dic.items():
-            heapq.heappush(priority_queue,(eye_v+hair_v,(eye_clr,hair_clr)))
-            # for skin_clr, skin_v in skin.items():
-            #     heapq.heappush(priority_queue,(eye_v+hair_v+skin_v,(eye_clr,hair_clr,skin_clr)))
-
-    while priority_queue:
-        diff, comb = heapq.heappop(priority_queue)
-        if comb in results:
-            print(results[comb])
-            print("Color detection error: ",diff)
+        while priority_queue:
+            diff, comb = heapq.heappop(priority_queue)
+            if comb[0] == comb[1]:
+                print("Case {}".format(T))
+                print("\tGT: ", ground_truth[str(T)].lower())
+                print("\tResult:",comb[0])
+                print("\tDiff:",diff)
+                if ground_truth[str(T)].lower() == comb[0]:
+                    correct += 1
+                else:
+                    wrong += 1
+                break
+    print("Correct: ",correct)
+    print("Wrong: ", wrong)
+    print("============================================")
